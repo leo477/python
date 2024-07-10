14c8a59bf34f205493c1423304b755b62a72fe84
Dmytro Zakharchenko <dmytrozakharrchenko@gmail.com>
2024-07-10 20:51:36 +0000

    First Task

--- PythonHomeWork/my_cv.py
+++ PythonHomeWork/my_cv.py
@@ -1,27 +1,29 @@
 from datetime import date
 
-born=date(1993,8,3)
-today=date.today()
-name="Dimka"
+born = date(1993, 8, 3)
+today = date.today()
+name = "Dimka"
 
-surname="Zakharchenko"
-name="Dmytro"
-languages=["Ukranian","English","Russian with translator"]
-position="BigData Analytic"
-food={"tomato","oranges","milk", "fish","fish"}
+surname = "Zakharchenko"
+name = "Dmytro"
+languages = ["Ukranian", "English", "Russian with translator"]
+position = " BigData Analytic"
+food = {"tomato", "oranges", "milk", "fish", "fish"}
 
 print("Hi, I'm " + name + " " + surname)
-print(f"I'm {(today-born).days//365} years old")
+print(f"I'm {(today - born).days // 365} years old")
 print("I speak:")
 for l in languages:
-    if l!=languages[2]:
+    if l != languages[2]:
         print(l)
 print("What food i like to have for dinner?")
 for f in food:
     match f:
-        case "oranges" : print(f.upper())
-        case _:    print(f)
-flag="programmer" in position
+        case "oranges":
+            print(f.upper())
+        case _:
+            print(f)
+flag = "programmer" in position
 if not flag:
     print("I work as " + position)
 else:
