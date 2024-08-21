a892c4076f4d6d24af461c7eb5e94b0d2ec6bc68
dmytro <zaharr3@gmail.com>
2024-08-21 17:13:16 +0300

    homework12

--- PythonHomeWork/homework12.py
+++ PythonHomeWork/homework12.py
@@ -7,20 +7,20 @@ data = response.json()
 
 df = pd.DataFrame(data)
 print(df.dtypes)
-df['calcdate']=pd.to_datetime(df['calcdate'],format='%d.%m.%Y')
-df['rate_per_unit']=df['rate_per_unit'].astype(float)
+df['calcdate'] = pd.to_datetime(df['calcdate'], format='%d.%m.%Y')
+df['rate_per_unit'] = df['rate_per_unit'].astype(float)
 print(df.head())
 print(df.dtypes)
 print(df.isnull().sum())
 df['rate_per_unit'] = df.groupby('cc')['rate_per_unit'].transform(lambda x: x.fillna(x.mean()))
+
 print(df.isnull().sum())
 df.dropna(inplace=True)
 top_currencies = df['cc'].value_counts().head(5).index
 df_filtered = df[df['cc'].isin(top_currencies)].copy()
 range_df = df_filtered.groupby('cc')['rate_per_unit'].agg(['max', 'min'])
 range_df['diff_min_max'] = range_df['max'] - range_df['min']
-df_final=df_filtered.merge(range_df['diff_min_max'], on='cc', how='left').copy()
-print(df_final.columns)
+
 grouped = df_filtered.merge(range_df['diff_min_max'], on='cc', how='left').groupby('cc')
 ga = grouped.agg(
     mean_rate=('rate_per_unit', 'mean'),
@@ -34,8 +34,3 @@ min_mean_currency = ga['mean_rate'].idxmin()
 print(f'Валюта з найвищим середнім курсом: {max_mean_currency}')
 print(f'Валюта з найнижчим середнім курсом: {min_mean_currency}')
 print(ga)
-
-
-
-
-
